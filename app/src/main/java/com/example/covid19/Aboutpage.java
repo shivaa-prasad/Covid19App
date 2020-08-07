10cefed40936ef6319c59643fbd307bef014fd10
Deeksha2308 <68339741+Deeksha2308@users.noreply.github.com>
2020-08-07 14:56:40 -0400

    Update Aboutpage.java

--- app/src/main/java/com/example/covid19/Aboutpage.java
+++ app/src/main/java/com/example/covid19/Aboutpage.java
@@ -10,5 +10,6 @@ public class Aboutpage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutpage);
+        //deeksha//
     }
 }
